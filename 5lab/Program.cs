9f54f1c051fbaf71ccbc70d420ae4bcdf1d9bb25
Дмитрий Савельев <mrpresident.evmount@gmail.com>
2020-10-26 21:12:47 +0300

    Первое задание

--- 5lab/Program.cs
+++ 5lab/Program.cs
@@ -6,10 +6,71 @@ using System.Threading.Tasks;
 
 namespace _5lab
 {
+
+    public interface IMovable
+    {
+        bool IsThereAnEngine();
+    }
+    public class Vehicle
+    {
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    public class Car : Vehicle, IMovable
+    {
+        public Car(int Power)
+        {
+            Engine engine = new Engine(Power);
+        }
+        public bool IsThereAnEngine()
+        {
+            return true;
+        }
+        public override string ToString()
+        {
+            string Str = "_5lab.Car Override";
+            return Str;
+        }
+    }
+    
+    public class Train : Vehicle, IMovable
+    {
+        public bool IsThereAnEngine()
+        {
+            return true;
+        }
+    }
+
+    public class Wagon : Train, IMovable
+    {
+
+    }
+
+    public class Express : Train, IMovable
+    {
+
+    }
+
+    sealed class Engine
+    {
+        int Power;
+        public Engine(int Power)
+        {
+            this.Power = Power;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Vehicle vehicle = new Vehicle();
+            Console.WriteLine(vehicle.ToString());
+            Car car = new Car(200);
+            Console.WriteLine(car.ToString());
         }
     }
 }
